6c76a9801942099cf1af8711151e58f624ac0e12
jphire <janne.laukkanen83@gmail.com>
2016-11-26 11:45:08 +0200

    update DHT library to use Sparkfun lib

--- arduino-measure-pusher.ino
+++ arduino-measure-pusher.ino
@@ -1,11 +1,13 @@
 
 #include <SPI.h>
 #include <Ethernet.h>
-#include <dht11.h>
+#include <DHT.h>
 #include <Arduino.h>
 
-dht11 DHT11;
-#define DHT11PIN 2
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
@@ -14,126 +16,119 @@ byte mac[] = { 0x20, 0x6A, 0x8A, 0x56, 0x94, 0x58 };
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(10,42,1,43);
-int serverPort = 80;
 EthernetClient client;
 
-long lastReadTime = 0;        // the last time you read the sensor, in ms
-long lastConnectionTime = 0;        // last time you connected to the server, in milliseconds
-boolean lastConnected = false;      // state of the connection last time through the main loop
-const int postingInterval = 20*1000;  //delay between updates to server
+long lastReadTime = 0;                // Last time you read the sensor, in ms
+long lastConnectionTime = 0;          // Last time you connected to the server, in milliseconds
+boolean lastConnected = false;        // State of the connection last time through the main loop
+const int postingInterval = 20*1000;  // Delay between updates to server
 
-// for the push-url
-char queryString[92];
-
-// set the ids for the user's data series
+// Set the ids for the user's data series
 char user_id[] = "11";
 char channel_id[] = "-JRCS3Zw9H7aUPGCVKk2";
 char temp_id[] = "-JRCSAE0hXqLCwCRPQ5S";
 char hum_id[] = "-JRCSEN0YW-5rc8SKwti";
 char dew_id[] = "-JRPRFJSDRzQxypL89MV";
+char serverUrl[] = "exactumgh.herokuapp.com";
+int serverPort = 80;
+
+// Querystring part for the url
+char queryString[92];
+char serverQuery[] = "GET /index.php?user=%s&channel=%s&series=%s&value=%s";
 
 char tempString[] = "00.00";
 char t_str[33];
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
   delay(500);
 
   // Start ethernet
-//  Serial.println(F("Starting ethernet..."));
-//  if (Ethernet.begin(mac) == 0) {
-//    Serial.println("Failed to configure Ethernet using DHCP");
+  Serial.println(F("Starting ethernet..."));
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
 //    // no point in carrying on, so do nothing forevermore:
-//    // try to congifure using IP address instead of DHCP:
-//    Ethernet.begin(mac, ip);
-//  }
+//    // try to configure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
 
-//  Serial.println(Ethernet.localIP());
+  Serial.print("Local IP: ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
   long currentTime = millis();
-
+  
+  // Check if interval time has passed after last data push
   if (currentTime > lastReadTime + postingInterval) {
-    checkSensor();
-    float temperature = (float)DHT11.temperature;
-    float humidity = (float)DHT11.humidity;
-    float dew_point = (float)dewPointFast(temperature, humidity);
-    Serial.print("dewpoint: ");
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    float dew_point = (float)dewPointFast(t, h);
+
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    Serial.print("temp, humidity, dewpoint: ");
+    Serial.print(t);
+    Serial.print(" ");
+    Serial.print(h);
+    Serial.print(" ");
     Serial.println(dew_point);
-    // if you're not connected, and the interval has passed since
-    // your last connection, then connect again and send data:
-//    if(!client.connected()) {
-//      sendData(user_id, channel_id, temp_id, temperature);
-//      sendData(user_id, channel_id, hum_id, humidity);
-//      sendData(user_id, channel_id, dew_id, dew_point);
-//    }
-    // update the time of the most current reading:
+    
+    // If not connected, and the interval has passed since
+    // last connection, connect again and send data
+    if (!client.connected()) {
+      sendData(user_id, channel_id, temp_id, t);
+      sendData(user_id, channel_id, hum_id, h);
+      sendData(user_id, channel_id, dew_id, dew_point);
+    }
+    // Update the time of the most current reading
     lastReadTime = millis();
   }
 
-  // if there's incoming data from the net connection.
+  // If there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
-//  if (client.available()) {
-//    char c = client.read();
-//    Serial.write(c);
-//  }
+  if (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
   
-  // if there's no net connection, but there was one last time
-  // through the loop, then stop the client:
-//  if (!client.connected() && lastConnected) {
-//    Serial.println();
-//    Serial.println("disconnecting.");
-//    client.stop();
-//  }
+  // If there's no connection, but there was one last time
+  // through the loop, stop the client:
+  if (!client.connected() && lastConnected) {
+    Serial.println();
+    Serial.println("Disconnecting.");
+    client.stop();
+  }
 
-  // store the state of the connection for next time through
+  // Save the state of the connection for next time through
   // the loop:
-//  lastConnected = client.connected();
+  lastConnected = client.connected();
 }
 
 byte sendData(char* user, char* channel, char* series, float value) {
   if(client.connected()) client.stop();
 
-  if(client.connect("exactumgh.herokuapp.com",80)) {
-    Serial.println("connecting..");
+  if (client.connect(serverUrl, serverPort)) {
+    Serial.println("Connected to remote host");
  
-    // convert the value to string
-    sprintf(queryString,"GET /index.php?user=%s&channel=%s&series=%s&value=%s",user,channel,series,ftoa(tempString,value,2));
+    // Convert the value to string
+    sprintf(queryString, serverQuery, user, channel, series, ftoa(tempString, value, 2));
     Serial.println(queryString);
+    
+    // Close the connection immediately
     client.print(queryString);
     client.println(" HTTP/1.1");
-    client.println("Host: exactumgh.herokuapp.com");
+    client.print("Host: ");
+    client.println(serverUrl);
     client.println("User-Agent: arduino-ethernet");
     client.println("Connection: close");
     client.println();
   } else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-  }
-}
-
-void checkSensor(){
-  Serial.println("\n");
-
-  int chk = DHT11.read(DHT11PIN);
-
-  Serial.print("Read sensor: ");
-  switch (chk)
-  {
-    case DHTLIB_OK: 
-		Serial.println("OK"); 
-		break;
-    case DHTLIB_ERROR_CHECKSUM: 
-		Serial.println("Checksum error"); 
-		break;
-    case DHTLIB_ERROR_TIMEOUT: 
-		Serial.println("Time out error"); 
-		break;
-    default: 
-		Serial.println("Unknown error"); 
-		break;
+    Serial.println("Connection failed");
   }
 }
 
